0d7792ea3c1bbd8f54350e24d0313be8eafba011
noahweiss890 <nweiss890@gmail.com>
2021-12-29 16:49:57 +0200

    made main

--- algo.h
+++ algo.h
@@ -3,9 +3,12 @@
 
 #include "nodes.h"
 
-pnode create_empty_graph(int amount);
-void delete_graph(pnode *head);
-void printGraph_cmd(pnode *head); //for self debug
-int shortest_path(int src, int dest);
-int tsp(char *string);
+void build_graph_cmd(pnode *head);
+void insert_node_cmd(pnode *head);
+void delete_node_cmd(pnode *head);
+void printGraph_cmd(pnode head); //for self debug
+void deleteGraph_cmd(pnode* head);
+void shortsPath_cmd(pnode head);
+void TSP_cmd(pnode head);
+
 #endif
