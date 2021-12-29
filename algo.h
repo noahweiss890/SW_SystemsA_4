6035b160ad7853ca633fea2c98e368d838f9deb2
Rashipachino <rashipachino@gmail.com>
2021-12-29 16:12:09 +0200

    algo, nodes, edges

--- algo.h
+++ algo.h
@@ -4,9 +4,8 @@
 #include "nodes.h"
 
 pnode create_empty_graph(int amount);
-void add_node(int id, pedge outedges, pnode nextNode);
 void delete_graph(pnode *head);
-void printGraph_cmd(pnode head); //for self debug
+void printGraph_cmd(pnode *head); //for self debug
 int shortest_path(int src, int dest);
 int tsp(char *string);
 #endif
