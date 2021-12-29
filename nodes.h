9921ea846f99244eadfe66b57a30aa353f8925ae
Rashipachino <rashipachino@gmail.com>
2021-12-29 15:51:45 +0200

    algo, nodes, edges

---
+++ nodes.h
@@ -0,0 +1,17 @@
+#ifndef GRAPH_NODE_
+#define GRAPH_NODE_
+
+#include "edges.h"
+
+typedef struct GRAPH_NODE_ {
+    int node_num;
+    pedge edges;
+    pnode next;
+} node, *pnode;
+
+void delete_node(int id);
+pnode get_node(pnode *head, int x);
+
+
+
+#endif
