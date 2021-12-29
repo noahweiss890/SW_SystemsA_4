8e7cac7bc900993b1461b2e72ec9975ec331f874
Rashipachino <rashipachino@gmail.com>
2021-12-29 17:23:57 +0200

    fixed some things

--- nodes.c
+++ nodes.c
@@ -2,18 +2,21 @@
 #include "edges.h"
 
 pnode get_node(pnode *head, int x){
+    pnode node;
     // Base case
     if (head == NULL)
-        return -1;
+        return NULL;
      
     // If key is present in current node, return true
     if ((*head)->node_num == x)
-        
+        return *head;
  
     // Recur for remaining list
     return search((*head)->next, x);
 }
-}
+
+
+
 
 
 
