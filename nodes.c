ea7a4480d886d13363369266314a8223bbf87a13
noahweiss890 <nweiss890@gmail.com>
2021-12-30 16:26:26 +0200

    debugging

--- nodes.c
+++ nodes.c
@@ -1,62 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "nodes.h"
 
-pnode get_node(pnode *head, int x) {
-    pnode node;
-    // Base case
-    if (head == NULL)
-    return NULL;
-    // If key is present in current node, return true
-    if ((*head)->node_num == x)
-        return *head;
-    // Recur for remaining list
-    return get_node((*head)->next, x);
+pnode get_node(pnode *head, int id) {
+    if(head == NULL) {
+        return NULL;
+    }
+    pnode curr = *head;
+    while(curr != NULL && curr->node_num != id) {
+        curr = curr->next;
+    }
+    return curr;
 }
 
-pnode create_node(pnode *head) {
-    int new_node_id = (int)getchar();
+void create_node(pnode *head) {
+    int new_node_id = (int)getchar() - '0';
     getchar();
-    pnode new_node = (pnode)malloc(sizeof(node));
-    if(new_node == NULL) {
-        printf("Not enough memory!");
-        exit(0);
-    }
-    new_node->node_num = new_node_id;
+    pnode new_node = get_node(head, new_node_id);
     new_node->edges = NULL;
     pedge prev = NULL;
     char dest = getchar();
     getchar();
     while(isdigit(dest)) {
-        int weight = (int)getchar();
+        int weight = (int)getchar() - '0';
         getchar();
         pedge new_edge = (pedge)malloc(sizeof(edge));
         if(new_node == NULL) {
             printf("Not enough memory!");
             exit(0);
         }
-        new_edge->endpoint = get_node(head, (int)getchar());
-        getchar();
-        new_edge->weight = (int)getchar();
-        getchar();
-        if(prev == NULL) {
+        new_edge->endpoint = get_node(head, (int)dest - '0');
+        new_edge->weight = weight;
+        if(new_node->edges == NULL) {
             new_node->edges = new_edge;
-            prev = new_node->edges;
+            prev = new_edge;
         }
         else {
             prev->next = new_edge;
+            prev = prev->next;
         }
-        prev = prev->next;
         dest = getchar();
         getchar();
     }
+    ungetc(' ', stdin);
     ungetc(dest, stdin);
-    return new_node;
 }
 
 void add_node(pnode *head, pnode new_node) {
-    pnode curr = head;
-    while(curr->next != NULL) {
-        curr = curr->next;
+    printf("before head: %p\n", head);
+    if(head == NULL) {
+        // printf("head: %p\n", head);
+        head = new_node;
+        printf("new head: %p\n", head);
+        // exit(0);
     }
-    curr->next = new_node;
+    else {
+        pnode curr = *head;
+        while(curr->next != NULL) {
+            curr = curr->next;
+        }
+        curr->next = new_node;
+    }
 }
